496434690fcbcdf94d3c11b4df80431698df8d29
Ezinwa charles <charleskelvin12@gmail.com>
2023-01-22 08:25:32 -0800

    Air

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -7,7 +7,7 @@ from fabric.api import local
 from fabric.api import put
 from fabric.api import run
 
-env.hosts = ["52.91.121.146", "3.85.136.181"]
+env.hosts = ["54.160.85.72", "35.175.132.106"]
 
 
 def do_pack():
@@ -73,4 +73,3 @@ def deploy():
     if file is None:
         return False
     return do_deploy(file)
-
